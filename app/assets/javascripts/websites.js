2f33920c8485120f8daf383afeded59b7c5c8b92
partheey <parthi1993@gmail.com>
2016-09-09 03:20:08 +0530

    Header added & Clientside URL validation implemented

--- app/assets/javascripts/websites.js
+++ app/assets/javascripts/websites.js
@@ -1,15 +1,22 @@
 var ready = function () {
-  $('#url_field').keyup(perform_validation(this));
-
-  function perform_validation(element) {
-    console.log($(element).val())
-    var url_validity = IsvalidURL($(element).val())
-    if (url_validity) {
-      element.class = 'form-group has-feedback has-success'
+  $('#url_field').keyup(function() {
+    var url = $(this).val()
+    if (IsvalidURL(url)) {
+      $(this).parent().attr("class",'form-group has-feedback has-success');
     } else {
-      element.class = 'form-group has-feedback has-error'
+      $(this).parent().attr("class",'form-group has-feedback has-error');
     }
-  }
+  });
+
+  $("#form_url").submit(function(e){
+    url = $('#url_field').text();
+    if (IsvalidURL(url)) {
+      console.log($(this).val());
+      e.submit();
+    } else {
+      e.preventDefault();
+    }
+  });
 
   function IsvalidURL(textval) {
     var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
